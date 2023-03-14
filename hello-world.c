866e6162672b43a397c2e33017f33e6d9560111c
George C <g9c9@outlook.com>
2023-03-13 17:55:12 -0700

    Setup compiling and debugging

---
+++ hello-world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void printHelloWorld() {
+    printf("Hello World!\n");
+}
+
+int main() {
+    printHelloWorld();
+    int num = 3;
+    printf("The num is %d", num);
+    return 0;
+}
